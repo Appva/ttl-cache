40166178c31e05a908eb641e8232d25fa465aef3
Teo Klestrup Röijezon <teo@nullable.se>
2021-01-29 14:28:09 +0100

    Extracted from pushapi

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use futures::{TryFuture, TryFutureExt};
+use std::time::SystemTime;
+
+/// Keeps a single value
+pub enum TtlCache<T> {
+    Empty,
+    Filled { value: T },
+}
+
+pub trait HasTtl {
+    fn valid_until(&self) -> SystemTime;
+}
+
+impl<T> Default for TtlCache<T> {
+    fn default() -> Self {
+        TtlCache::Empty
+    }
+}
+
+impl<T> TtlCache<T> {
+    fn insert(&mut self, value: T) -> &mut T {
+        *self = TtlCache::Filled { value };
+        match self {
+            TtlCache::Filled { value } => value,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl<T: HasTtl> TtlCache<T> {
+    fn clear_if_expired(&mut self, now: SystemTime) {
+        if let TtlCache::Filled { value } = self {
+            if now > value.valid_until() {
+                *self = TtlCache::Empty
+            }
+        }
+    }
+
+    /// Immediately returns (a reference to) the current value if it is still valid,
+    /// otherwise calls `update` to refresh it. Only [`Ok`] results are cached.
+    pub async fn try_get_or_update<Fut: TryFuture<Ok = T>>(
+        &mut self,
+        update: impl FnOnce() -> Fut,
+    ) -> Result<&mut T, Fut::Error> {
+        self.clear_if_expired(SystemTime::now());
+        Ok(match self {
+            TtlCache::Empty => self.insert(update().into_future().await?),
+            TtlCache::Filled { value } => value,
+        })
+    }
+}
